fe3974c9bcb6b5288a201e61c45d995a6a7172a3
RealTitanGod <48302092+RealTitanGod@users.noreply.github.com>
2025-10-29 19:49:03 -0400

    v2.1.2

--- SuperFastBuildingAndLeveling/Settings.cs
+++ SuperFastBuildingAndLeveling/Settings.cs
@@ -82,7 +82,7 @@ namespace SuperFastBuildingAndLeveling
         public string Mod => "Super Fast Building And Leveling";
 
         [SettingsUISection(InfoTab, InfoGroup)]
-        public string Version => "2.1.1";
+        public string Version => "2.1.2";
 
         [SettingsUISection(InfoTab, InfoGroup)]
         public string Developer => "TitanGod_";
